7c7cd4cc87c0b04a6d4372ea34f42d4ea4d9b2dc
Paulo Moreira <phmoreira37@gmail.com>
2023-05-20 10:43:03 -0300

    feat: add fonts Roboto & BaiJamjuree with expo
    lib: expo-google-fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="text-5xl font-bold text-gray-50">Hello World!</Text>
+      <Text className="font-alt text-5xl font-bold text-gray-50">
+        Hello World!
+      </Text>
       <StatusBar style="light" translucent />
     </View>
   )
