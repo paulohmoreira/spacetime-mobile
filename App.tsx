bfa5b65bb2d614236fc340caea87d8f10ccaa99e
Paulo Moreira <phmoreira37@gmail.com>
2023-05-20 11:22:35 -0300

    feat: Add Stripes and main element

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { styled } from 'nativewind'
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
-
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/logo.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -24,9 +29,33 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
